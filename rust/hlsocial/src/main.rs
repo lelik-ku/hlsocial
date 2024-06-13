8987b7056d30280362a40f0224a02427ca4a8edd
lelik-ku <forinfos@list.ru>
2024-06-13 21:38:40 +0300

    task2

--- rust/hlsocial/src/main.rs
+++ rust/hlsocial/src/main.rs
@@ -19,17 +19,22 @@ async fn main() -> std::io::Result<()> {
 
     // PSQL Connection
     let pgurl = format!("postgres://{}:{}@{}:{}/{}", config.db_user, config.db_pass, config.db_host, config.db_port, config.db_name);
-    let pool = PgPoolOptions::new().max_connections(5).connect(&pgurl).await;
+    let pool = PgPoolOptions::new().max_connections(99).connect(&pgurl).await;
     println!("INFO: Connecting to PostgreSQL server: {}@{}:{}...", config.db_user, config.db_host, config.db_port);
     let pool = match pool {
         Ok(p) => p,
         Err(e) => panic!("ERROR: Can't connect to PostgreSQL server: {}@{}:{} due to: {:?}", config.db_user, config.db_host, config.db_port, e)
     };
     println!("INFO: Applying migrations...");
-    let _ = match sqlx::migrate!("./migrations").run(&pool).await {
-        Ok(m) => m,
-        Err(e) => panic!("ERROR: Can't apply migrations to PostgreSQL server: {}@{}:{} due to: {:?}", config.db_user, config.db_host, config.db_port, e)
-    };
+    sqlx::migrate!("./migrations").run(&pool).await.unwrap_or_else(
+        |err| {
+            panic!("ERROR: Can't apply migrations to PostgreSQL server: {}@{}:{} due to: {:?}",
+            config.db_user,
+            config.db_host,
+            config.db_port,
+            err)
+        }
+    );
 
     // API Server
     api::start_server(config, pool).await
